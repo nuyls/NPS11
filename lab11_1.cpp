04abf88e7a0ada90e8ec5f843d8035e2f5a342b0
NpS <preeyanan.nps@gmail.com>
2023-12-22 14:56:26 +0700

    11.1

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,24 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+using namespace std;
+
+int main() {
+    cout << "Press Enter 3 times to reveal your future.";
+
+    for (int i = 0; i < 3; ++i) {
+        cin.get();
+    }
+
+    srand(time(0));
+
+    string grades[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+
+    int random = rand() % 9;
+    string Grade = grades[random];
+
+    cout << "You will get " << Grade <<" in this 261102." << endl;
+
+    return 0;
+}
